9d68e76741dac7549cdc72e90ef361f9554970bf
Malarkodimarimuthu <malarsuba7675@gmail.com>
2026-01-22 13:27:30 +0530

    Create string program.java

---
+++ string program.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int num = sc.nextInt();
+        float num2 = sc.nextFloat();
+        System.out.println(num);
+            System.out.printf("%.2f",num2);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
