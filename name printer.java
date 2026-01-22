a424e57d45e9b8502e58607abb38b240bc4399a6
Malarkodimarimuthu <malarsuba7675@gmail.com>
2026-01-22 13:22:06 +0530

    Create name printer.java

---
+++ name printer.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class name printer {
+
+    public static void main(String[] args) {
+        Scanner sc = nextLinew Scanner (System.in);
+        String name = sc.();
+        System.out.println("Hello\n" +name);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
